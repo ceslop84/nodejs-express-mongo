d5457ebc605d377c126d6b9ba311a8e7c7b7e4ca
Cesar Rafael Lopes <cesar.lopes@serpro.gov.br>
2024-04-05 14:42:17 -0300

    Aula 3

---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import LivroController from "../controllers/livroController.js";
+
+const routes = express.Router();
+
+routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/:id", LivroController.listarLivro);
+routes.put("/livros/:id", LivroController.atualizarLivro);
+routes.delete("/livros/:id", LivroController.apagarLivro);
+routes.post("/livros", LivroController.cadastrarLivro);
+
+export default routes;
