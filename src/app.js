81dcf91ba8778c7999d43f2ea1bde47b3d466686
Cesar Rafael Lopes <cesar.lopes@serpro.gov.br>
2024-04-05 11:58:34 -0300

    Carga de dados.

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import express from "express";
+import conectaBancoDados from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaBancoDados();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexão com o banco realizada com sucesso.");
+});
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", async (req, res) => {
+  const listaLivros = await livro.find({});
+  res.status(200).json(listaLivros);
+});
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso!");
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("Livro excluído com sucesso.");
+});
+
+export default app;
